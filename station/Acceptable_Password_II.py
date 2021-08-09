10d4b47af5adeba8c1e46b09bc4ae9347de3dd55
laikov <zheka-se@ya.ru>
2021-08-09 10:59:27 +0300

    09-08

---
+++ station/Acceptable_Password_II.py
@@ -0,0 +1,22 @@
+"""
+ In this mission you need to create a password verification function.
+
+Those are the verification conditions:
+
+    the length should be bigger than 6;
+    should contain at least one digit.
+
+Input: A string.
+
+Output: A bool.
+"""
+import re
+def is_acceptable_password(password: str) -> bool:
+    if len(password) > 6 and any(map(str.isdigit, password)):
+        return True
+    return False
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_acceptable_password('dddsdd'))
